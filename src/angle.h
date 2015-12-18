016b42d2c76b21f8eab0cf8074ccdb6735745780
laurent.saboret <laurent.saboret@67464621-8d75-4bde-9f6c-a8f136609eee>
2015-12-18 10:26:10 +0000

    Correction du bug #306 GTI avec tracking 1-axe
    git-svn-id: http://svn.oie-lab.net/trunk/codes/solar_geometry@3764 67464621-8d75-4bde-9f6c-a8f136609eee

--- src/angle.h
+++ src/angle.h
@@ -10,10 +10,16 @@
 
 #include <math.h>
 #include <float.h>
+#include <assert.h>
 
 #ifndef M_PI
 #define M_PI 3.141592653589793
 #define M_PI_2 1.570796326794897
+#endif
+
+/* An expression representing a value which is “not a number”. This macro is a GNU extension predefined for C99 and C++. */
+#ifndef NAN
+#define NAN  (__builtin_nanf(""))
 #endif
 
 #ifdef __cplusplus
@@ -30,6 +36,8 @@ extern "C"
 #else
     #define INLINE inline
 #endif
+
+
 /* This structure represents an angle expressed in radians.
  * Calls to cos(), sin(), tan() are cached.
  * 
@@ -46,12 +54,14 @@ typedef struct {
 static INLINE angle_t init_angle(double angle) { // angle in radians
     angle_t ths;
     
+    assert(!isnan(angle));
+    
     ths.angle     = angle;
     
     /* other fields are not yet computed */
-    ths.cos_angle = DBL_MAX;
-    ths.sin_angle = DBL_MAX;
-    ths.tan_angle = DBL_MAX;
+    ths.cos_angle = NAN;
+    ths.sin_angle = NAN;
+    ths.tan_angle = NAN;
     
     return ths;
 }
@@ -59,12 +69,14 @@ static INLINE angle_t init_angle(double angle) { // angle in radians
 static INLINE angle_t init_angle_cos(double angle, double cos_angle) {
     angle_t ths;
     
+    assert(!isnan(angle));
+    
     ths.angle     = angle;
     ths.cos_angle = cos_angle;
     
     /* other fields are not yet computed */
-    ths.sin_angle = DBL_MAX;
-    ths.tan_angle = DBL_MAX;
+    ths.sin_angle = NAN;
+    ths.tan_angle = NAN;
     
     return ths;
 }
@@ -72,12 +84,29 @@ static INLINE angle_t init_angle_cos(double angle, double cos_angle) {
 static INLINE angle_t init_angle_sin(double angle, double sin_angle) {
     angle_t ths;
     
+    assert(!isnan(angle));
+    
     ths.angle     = angle;
     ths.sin_angle = sin_angle;
     
     /* other fields are not yet computed */
-    ths.cos_angle = DBL_MAX;
-    ths.tan_angle = DBL_MAX;
+    ths.cos_angle = NAN;
+    ths.tan_angle = NAN;
+    
+    return ths;
+}
+
+static INLINE angle_t init_angle_tan(double angle, double tan_angle) {
+    angle_t ths;
+    
+    assert(!isnan(angle));
+    
+    ths.angle     = angle;
+    ths.tan_angle = tan_angle;
+    
+    /* other fields are not yet computed */
+    ths.cos_angle = NAN;
+    ths.sin_angle = NAN;
     
     return ths;
 }
@@ -85,12 +114,14 @@ static INLINE angle_t init_angle_sin(double angle, double sin_angle) {
 static INLINE angle_t init_angle_cos_sin(double angle, double cos_angle, double sin_angle) {
     angle_t ths;
     
+    assert(!isnan(angle));
+    
     ths.angle     = angle;
     ths.cos_angle = cos_angle;
     ths.sin_angle = sin_angle;
     
     /* other fields are not yet computed */
-    ths.tan_angle = DBL_MAX;
+    ths.tan_angle = NAN;
     
     return ths;
 }
@@ -99,10 +130,10 @@ static INLINE angle_t init_angle_cos_sin(double angle, double cos_angle, double 
 static INLINE angle_t minus_angle(const angle_t* a) {
     angle_t ths;
     
-    ths.angle     = a->angle;
+    ths.angle     = -a->angle;
     ths.cos_angle = a->cos_angle;
-    ths.sin_angle = (a->sin_angle != DBL_MAX) ? (-a->sin_angle) : DBL_MAX;
-    ths.tan_angle = (a->tan_angle != DBL_MAX) ? (-a->tan_angle) : DBL_MAX;
+    ths.sin_angle = -a->sin_angle;
+    ths.tan_angle = -a->tan_angle;
     
     return ths;
 }
@@ -114,7 +145,43 @@ static INLINE angle_t pi_2_minus_angle(const angle_t* a) {
     ths.angle     = M_PI_2 - a->angle;
     ths.cos_angle = a->sin_angle;
     ths.sin_angle = a->cos_angle;
-    ths.tan_angle = (a->tan_angle != DBL_MAX) ? (1.0/a->tan_angle) : DBL_MAX;
+    ths.tan_angle = 1.0/a->tan_angle;
+    
+    return ths;
+}
+
+// Return a + PI 
+static INLINE angle_t angle_plus_pi(const angle_t* a) {
+    angle_t ths;
+    
+    ths.angle     = a->angle + M_PI;
+    ths.cos_angle = -a->cos_angle;
+    ths.sin_angle = -a->sin_angle;
+    ths.tan_angle = a->tan_angle;
+    
+    return ths;
+}
+
+// Return a + 2*PI 
+static INLINE angle_t angle_plus_2pi(const angle_t* a) {
+    angle_t ths;
+    
+    ths.angle     = a->angle + 2.0*M_PI;
+    ths.cos_angle = a->cos_angle;
+    ths.sin_angle = a->sin_angle;
+    ths.tan_angle = a->tan_angle;
+    
+    return ths;
+}
+
+// Return a - 2*PI 
+static INLINE angle_t angle_minus_2pi(const angle_t* a) {
+    angle_t ths;
+    
+    ths.angle     = a->angle - 2.0*M_PI;
+    ths.cos_angle = a->cos_angle;
+    ths.sin_angle = a->sin_angle;
+    ths.tan_angle = a->tan_angle;
     
     return ths;
 }
@@ -124,30 +191,26 @@ static INLINE double get_angle(const angle_t* ths) {
     return ths->angle;
 }
 
-// compute cosinus only once
+// compute cosinus (cached)
 static INLINE double get_cos(const angle_t* ths) { 
-    if (ths->cos_angle == DBL_MAX)
+    if (isnan(ths->cos_angle))
         ((angle_t*)ths)->cos_angle = cos(ths->angle);
     
     return ths->cos_angle;
 }
 
-// compute sinus only once
+// compute sinus (cached)
 static INLINE double get_sin(const angle_t* ths) { 
-    if (ths->sin_angle == DBL_MAX)
+    if (isnan(ths->sin_angle))
         ((angle_t*)ths)->sin_angle = sin(ths->angle);
     
     return ths->sin_angle;
 }
 
-// compute tangent only once
+// compute tangent (cached)
 static INLINE double get_tan(const angle_t* ths) { 
-    if (ths->tan_angle == DBL_MAX) {
-        if (ths->cos_angle != DBL_MAX && ths->sin_angle != DBL_MAX)
-            ((angle_t*)ths)->tan_angle = ths->sin_angle / ths->cos_angle;
-        else
-            ((angle_t*)ths)->tan_angle = tan(ths->angle);
-    }
+    if (isnan(ths->tan_angle))
+        ((angle_t*)ths)->tan_angle = tan(ths->angle);
     
     return ths->tan_angle;
 }
