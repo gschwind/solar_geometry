a25e39e4ee1af6572ed85c0e599c146daa3afbd9
Benoit Gschwind <benoit.gschwind@mines-paristech.fr>
2020-06-25 09:38:26 +0200

    Update setup.py
    setup.py link with solargeometry library by default instead of build it
    inside the python binding. This patch also add the posibility to static
    link the solar_geometry library. It also remove the default -ggdb C
    flags.

--- setup.py
+++ setup.py
@@ -3,12 +3,51 @@
 
 from distutils.core import setup, Extension
 from subprocess import check_output
+from collections import defaultdict
 import numpy
+import os, sys
+
+def pkgconfig(*packages, **kw):
+ flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+ ret = defaultdict(list)
+ ret.update(kw)
+ for token in check_output(["pkg-config", "--libs", "--cflags"] + list(packages)).decode('UTF-8').split():
+  if token[:2] in flag_map:
+   ret[flag_map.get(token[:2])].append(token[2:])
+  else:
+   ret['extra_link_args'].append(token)
+ for k, v in kw.items(): # remove duplicated
+  ret[k] = list(set(ret[k]))
+ return ret
+
+params = pkgconfig('solar_geometry')
+params['extra_compile_args'] = ['-std=c++11']
+params["include_dirs"] += [numpy.get_include(), "third-parties/python-bind-helper", "src"]
+
+# This section try to find the required
+# static libraries
+if "--static" in sys.argv:
+    sys.argv.remove("--static")
+    new_libraries = []
+    for l in params["libraries"]:
+        static_lib_found = False
+        for p in params["library_dirs"]:
+            f = "%s/lib%s.a"%(p,l)
+            if os.path.exists(f):
+                params["extra_objects"].append(f)
+                static_lib_found = True
+                break
+        if not static_lib_found:
+            new_libraries.append(l)
+
+    params["libraries"] = new_libraries
+
+params["libraries"] = new_libraries
+
 
 solar_geometry_module = Extension('solar_geometry',
-    sources = ['python/pysolar_geometry.cxx', 'src/solar_geometry.cxx'],
-    extra_compile_args = ['-std=c++11', '-ggdb'],
-    include_dirs = [numpy.get_include(), "third-parties/python-bind-helper", "src"])
+    sources = ['python/pysolar_geometry.cxx'],
+    **params)
 
 setup(name = 'solar_geometry',
       version = '1.0',
